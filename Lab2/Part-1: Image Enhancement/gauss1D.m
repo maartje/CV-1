0cc0ecbfd08258d562ba86aed4fa9639c95a71de
maartje <maartjedejonge@gmail.com>
2018-02-23 10:08:34 +0100

    add matlab files for lab2

---
+++ Lab2/Part-1: Image Enhancement/gauss1D.m
@@ -0,0 +1,11 @@
+function G = gauss1D( sigma , kernel_size )
+    G = zeros(1, kernel_size);
+    if mod(kernel_size, 2) == 0
+        error('kernel_size must be odd, otherwise the filter will not have a center to convolve on')
+    end
+    %% solution
+    G = -1 * floor(kernel_size/2) : floor(kernel_size/2);
+    G = -(G .^ 2)/(2*sigma^2);
+    G = (1/(sigma*sqrt(2*pi))) * exp(G);
+    G = G / sum(G);
+end
