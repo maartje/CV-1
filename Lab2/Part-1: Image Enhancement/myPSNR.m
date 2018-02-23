4860d6017298666a796169b7af9bc3a5f8c81c5d
maartje <maartjedejonge@gmail.com>
2018-02-23 21:09:12 +0100

    implementing denoise and psnr

--- Lab2/Part-1: Image Enhancement/myPSNR.m
+++ Lab2/Part-1: Image Enhancement/myPSNR.m
@@ -1,5 +1,11 @@
 function [ PSNR ] = myPSNR( orig_image, approx_image )
-fprintf('Not implemented\n')
 
+orig_im = im2double(orig_image);
+approx_im = im2double(approx_image);
+Imax = max(orig_im(:));
+errs = (orig_im - approx_im) .^ 2;
+MSE = sum(errs(:)) / numel(orig_im);
+RMSE = sqrt(MSE);
+PSNR = 20 * log10(Imax / RMSE);
 end
 
