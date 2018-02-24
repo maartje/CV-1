c8dc15a4483ebd8213eb1eb74ac02537d7a7ac99
maartje <maartjedejonge@gmail.com>
2018-02-24 15:25:32 +0100

    process guassian filter type

--- Lab2/Part-1: Image Enhancement/main.m
+++ Lab2/Part-1: Image Enhancement/main.m
@@ -1,3 +1,7 @@
+clear
+clc
+close all
+
 im1 = imread('images/image1.jpg');
 im1_salt_pepper = imread('images/image1_saltpepper.jpg');
 im1_gaussian = imread('images/image1_gaussian.jpg');
@@ -10,6 +14,7 @@ fprintf('psnr %d "im1 - im1_gaussian" .\n\n', PSNR_q2);
 
 show_denoised_images('images/image1_saltpepper.jpg', ["box", "median"], [3,5,7]);
 show_denoised_images('images/image1_gaussian.jpg', ["box", "median"], [3,5,7]);
+show_denoised_images('images/image1_gaussian.jpg', ["gaussian"], [0.5,1,2]);
 
 function show_denoised_images(im_path, ftypes, fsizes)
 im = imread(im_path);
@@ -18,9 +23,17 @@ for ft_idx = 1:numel(ftypes)
     for fs_idx = 1:numel(fsizes)
         ft = ftypes(ft_idx);
         fs = fsizes(fs_idx);
-        denoised = denoise(im, ft, fs);
+        if ft == "gaussian"
+            fsize = ceil(4 * fs + 1); % use +- 4 times sigma + 1 as kernel size
+            if mod(fsize,2) == 0
+                fsize = fsize - 1; % kernel size must be odd
+            end
+            denoised = denoise(im, ft, fsize, fs);
+        else
+            denoised = denoise(im, ft, fs);
+        end
         plot_idx = (ft_idx - 1)*numel(fsizes) + fs_idx;
-        subplot(numel(ftypes),numel(fsizes), plot_idx);
+        subplot(numel(ftypes), numel(fsizes), plot_idx);
         imshow(denoised);
         fprintf('psnr %s %d:  %d. \n', ft, fs, myPSNR(denoised, im));
     end
