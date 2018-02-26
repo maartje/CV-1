891346be6d36d12e25c1774a4ab437b5a9b70486
maartje <maartjedejonge@gmail.com>
2018-02-26 10:32:14 +0100

    split main file in functions, implement LoG

--- Lab2/Part-1: Image Enhancement/compute_LoG.m
+++ Lab2/Part-1: Image Enhancement/compute_LoG.m
@@ -1,18 +1,23 @@
 function imOut = compute_LoG(image, LOG_type)
-
+im = im2double(image);
 switch LOG_type
     case 1
         %method 1
-        fprintf('Not implemented\n')
-
+        kguassian = fspecial('gaussian', 5, 0.5);
+        im_g = imfilter(im, kguassian, 'replicate');
+        klaplacian = fspecial('laplacian', 0.2);
+        imOut = imfilter(im_g, klaplacian, 'replicate');
     case 2
         %method 2
-        fprintf('Not implemented\n')
-
+        klog = fspecial('log', 5, 0.5);
+        imOut = imfilter(im, klog, 'replicate');
     case 3
         %method 3
-        fprintf('Not implemented\n')
-
+        kguassian1 = fspecial('gaussian', 5, 0.5);
+        im_g1 = imfilter(im, kguassian1, 'replicate');
+        kguassian2 = fspecial('gaussian', 5, 0.5);
+        im_g2 = imfilter(im, kguassian2, 'replicate');
+        imOut = im_g1 - im_g2;
 end
 end
 
--- Lab2/Part-1: Image Enhancement/main.m
+++ Lab2/Part-1: Image Enhancement/main.m
@@ -2,27 +2,27 @@ clear
 clc
 close all
 
-im1 = imread('images/image1.jpg');
-im1_salt_pepper = imread('images/image1_saltpepper.jpg');
-im1_gaussian = imread('images/image1_gaussian.jpg');
+question6();
+question7();
+question8();
+question9();
 
-PSNR_q1 = myPSNR(im1, im1_salt_pepper);
-PSNR_q2 = myPSNR(im1, im1_gaussian);
-
-fprintf('psnr %d "im1 - im1_salt_pepper" .\n', PSNR_q1);
-fprintf('psnr %d "im1 - im1_gaussian" .\n\n', PSNR_q2);
-
-show_denoised_images('images/image1_saltpepper.jpg', ["box", "median"], [3,5,7]);
-show_denoised_images('images/image1_gaussian.jpg', ["box", "median"], [3,5,7]);
-
-show_denoised_images('images/image1_saltpepper.jpg', ["gaussian"], [0.5,1,2]);
-show_denoised_images('images/image1_gaussian.jpg', ["gaussian"], [0.5,1,2]);
-
+function question9()
 image2 = imread('images/image2.jpg');
-compute_gradient(image2);
+im2_gl = compute_LoG(image2, 1);
+im2_log = compute_LoG(image2, 2);
+im2_dog = compute_LoG(image2, 3);
+figure;
+subplot(1,3,1);
+imshow(im2_gl), title('Laplace after Gaussian');
+subplot(1,3,2);
+imshow(im2_log), title('LoG filter');
+subplot(1,3,3);
+imshow(im2_dog), title('Dog filter');
+end
 
-[Gx, Gy] = imgradientxy(image2);
-[Gmag, Gdir] = imgradient(Gx, Gy);
+function question8()
+image2 = imread('images/image2.jpg');
 [Gx, Gy, Gmag, Gdir] = compute_gradient(image2);
 
 figure;
@@ -34,7 +34,29 @@ subplot(2,2,3);
 imshow(Gmag, []), title('Gradient magnitude');
 subplot(2,2,4);
 imshow(Gdir, []), title('Gradient direction');
+end
 
+function question7()
+show_denoised_images('images/image1_saltpepper.jpg', ["box", "median"], [3,5,7]);
+show_denoised_images('images/image1_gaussian.jpg', ["box", "median"], [3,5,7]);
+
+show_denoised_images('images/image1_saltpepper.jpg', ["gaussian"], [0.5,1,2]);
+show_denoised_images('images/image1_gaussian.jpg', ["gaussian"], [0.5,1,2]);
+
+end
+
+function question6()
+im1 = imread('images/image1.jpg');
+im1_salt_pepper = imread('images/image1_saltpepper.jpg');
+im1_gaussian = imread('images/image1_gaussian.jpg');
+
+PSNR_q1 = myPSNR(im1, im1_salt_pepper);
+PSNR_q2 = myPSNR(im1, im1_gaussian);
+
+fprintf('psnr %d "im1 - im1_salt_pepper" .\n', PSNR_q1);
+fprintf('psnr %d "im1 - im1_gaussian" .\n\n', PSNR_q2);
+
+end
 
 function show_denoised_images(im_path, ftypes, fsizes)
 fprintf('\n%s: \n', im_path);
