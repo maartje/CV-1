255bab4c979425e7deec10d6844ab59b66e1505c
maartje <maartjedejonge@gmail.com>
2018-03-13 12:56:38 +0100

    working on transform image

--- Lab4/main.m
+++ Lab4/main.m
@@ -58,45 +58,47 @@ function I_transformed = transform_image(im, M, T)
     min_xy = floor(min(I_xy_transformed, [], 2));
     I_xy_transformed = I_xy_transformed - [min_xy(1); min_xy(2); 0];
 
-    % build new image
+    % build matrix with pointers to nearest neighbor index in I_xy_transformed 
     max_xy = floor(max(I_xy_transformed, [], 2));
-    I_transformed = zeros(max_xy(2), max_xy(1));
+    I_transformed_indices = zeros(max_xy(2), max_xy(1));
     for i = 1 : size(I_xy_transformed, 2)
         xy = I_xy_transformed(1:2,i);
         x_coordinate = min(max(round(xy(1)), 1), max_xy(1)); % round to int and fit in image range
         y_coordinate = min(max(round(xy(2)), 1), max_xy(2));
-        nn_index = I_transformed(y_coordinate, x_coordinate);
+        nn_index = I_transformed_indices(y_coordinate, x_coordinate);
         if nn_index == 0
-            I_transformed(y_coordinate, x_coordinate) = i;
+            I_transformed_indices(y_coordinate, x_coordinate) = i;
         else
             xy_coordinate = [x_coordinate; y_coordinate];
             xy_prev = I_xy_transformed(1:2, nn_index);
             distance_prev_sq = transpose(xy_prev - xy_coordinate) * (xy_prev - xy_coordinate);
             distance_current_sq = transpose(xy - xy_coordinate) * (xy - xy_coordinate);            
             if distance_current_sq > distance_prev_sq
-                I_transformed(y_coordinate, x_coordinate) = i;
+                I_transformed_indices(y_coordinate, x_coordinate) = i;
             end
         end
     end
+    
+    
 
-%     for x = 1 : size(I_transformed, 2)
-%         col_indices = I_transformed(:, x);
-%         zero_col_indices = find(~col_indices);
-%         if ~isempty(zero_col_indices)
-%             for i = 1:length(zero_col_indices)
-%                 y = zero_col_indices(i);
-%                 for xx = x-1:x+1
-%                     for yy = y-1:y+1
-%                         
-%             end
-%         end
-%     end
-
-    for x = 1 : size(I_transformed, 2)
-        for y = 1 : size(I_transformed, 1)
-            nn_index = I_transformed(y,x);
-            if nn_index > 0
+    I_transformed = zeros(size(I_transformed_indices));
+    for x = 1 : size(I_transformed_indices, 2)
+        for y = 1 : size(I_transformed_indices, 1)
+            nn_index = I_transformed_indices(y,x);
+            if nn_index
                 I_transformed(y,x) = I_xy_transformed(3, nn_index);
+            else
+                % we assume that window 3x3 is enough to interpolate pixels in the middle
+                neigbor_indices = I_transformed_indices(max(y-1,1) : min(y+1,size(I_transformed_indices, 1)), max(x-1,1) : min(x+1, size(I_transformed_indices, 2)));
+                neigbor_indices = neigbor_indices(:);
+                neigbor_indices = neigbor_indices(neigbor_indices > 0);
+                if ~isempty(neigbor_indices)
+                    % we are lazy and pick a random neighbor instead of figuring out the 
+                    % nearest neighbor by comparing distances to neighboring points 
+                    % in I_xy_transform
+                    nn_index = neigbor_indices(1); 
+                    I_transformed(y,x) = I_xy_transformed(3, nn_index);
+                end
             end
         end
     end
