79ce4f91f6ad6624e6717b1b65444db613db3ce7
maartje <maartjedejonge@gmail.com>
2018-03-13 21:45:40 +0100

    visualize matching keypoints

--- Lab4/main.m
+++ Lab4/main.m
@@ -2,10 +2,10 @@
 close all
 clear
 
-demo_image_alignment('boat1.pgm', 'boat2.pgm');
+%demo_image_alignment('boat1.pgm', 'boat2.pgm');
 demo_image_alignment('boat2.pgm', 'boat1.pgm');
-demo_stitching('left.jpg', 'right.jpg');
-demo_stitching('right.jpg', 'left.jpg');
+%demo_stitching('left.jpg', 'right.jpg');
+%demo_stitching('right.jpg', 'left.jpg');
 
 function demo_stitching(impath1, impath2)
     % stitch two images
@@ -42,6 +42,9 @@ function demo_image_alignment(impath1, impath2)
     trform = affine2d(A);
     im1_to_im2_mtl = imwarp(im1, trform);
 
+    % TODO show images side by side with lines connecting the matches
+    visualize_matching_keypoints(kpmatches, im1, im2, f1, f2);
+
     % plot
     figure;
     subplot(2,2,1);
@@ -55,3 +58,42 @@ function demo_image_alignment(impath1, impath2)
 
 end
 
+function visualize_matching_keypoints(matches, im1, im2, f1, f2)
+
+    % concatenate the images    
+    h1 = size(im1, 1);
+    h2 = size(im2, 1);
+    h = max(h1, h2);
+    im1_padded = zeros(h, size(im1, 2), 'uint8');
+    im2_padded = zeros(h, size(im2, 2), 'uint8');    
+    im1_padded(1:size(im1, 1),1:size(im1, 2)) = im1;
+    im2_padded(1:size(im2, 1),1:size(im2, 2)) = im2;        
+    im_concatenated = cat(2, im1_padded, im2_padded);
+
+    % random sample of size 50
+    r = randperm(size(matches, 2), 50);
+    sample_matches = matches(:, r);
+
+    % select pixel coordinates of selected matches 
+    X_1 = f1(1,sample_matches(1,:));
+    Y_1 = f1(2,sample_matches(1,:));
+    X_2 = f2(1,sample_matches(2,:)) + size(im1, 2);
+    Y_2 = f2(2,sample_matches(2,:));
+
+    % Plot this
+    figure(1);
+    imshow(im_concatenated);
+
+    hold on;
+
+    % Create the lines
+    lines = plot([X_1; X_2], [Y_1;Y_2]);
+    set(lines,'color','y');
+
+    % create the points
+    vl_plotframe(f1(:, sample_matches(1,:)));
+    f2(1,:) = f2(1,:) + size(im1, 2);
+    vl_plotframe(f2(:, sample_matches(2,:)));
+
+    hold off;
+end
